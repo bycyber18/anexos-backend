7fd2e8127840ec8f24c2dcdc0d1afd2214ca9e20
Tommy <tsaavedrai@usm.cl>
2026-02-05 15:12:49 -0300

    Ahora hay login

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
+// Conectar a MongoDB
 connectDB();
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas Importadas
+// Rutas
 const anexoRoutes = require("./routes/anexo.routes");
+const authRoutes = require("./routes/auth.routes");
 
-// Uso de Rutas
+// Uso de rutas
 app.use("/api/anexos", anexoRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
